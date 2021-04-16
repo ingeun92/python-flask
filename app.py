0a0b60ddb3051dc4a99ef7d08d24496f3927582d
Ingeun Kim <ingeun92@naver.com>
2021-04-16 18:18:57 +0900

    Update app.py - connect with api.py

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+from flask_restful import Api
 
+import api
 
 app = Flask("Ing")
+api_init = Api(app)
+api_init.add_resource(api.Plus, "/plus")
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/flask-ing"
+mongo = PyMongo(app)
+collection = mongo.db.tmp
 
 
 @app.route("/")
 def index():
-    return "Hello World!"
+
+    return "Hello"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
